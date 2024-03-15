3619b2995a2904162b9053774b2cfabbcda32b51
Yashwardhan Shinde <yashwardhanshinde25@gmail.com>
2024-03-15 23:04:51 +0530

    Add files via upload

---
+++ Madlibs_generator.py
@@ -0,0 +1,29 @@
+with open("story1.txt", "r") as f:
+    story1 = f.read()
+
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story1):
+    if char == target_start:
+        start_of_word = i
+
+    if char == target_end and start_of_word != -1:
+        word = story1[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
+
+answers = {}
+
+for word in words:
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story1)
+
